5e5076587b1def0ac8226f8f7e488efc69c16a65
MD-Baki <bakimd77@gmail.com>
2022-10-25 21:44:11 +0600

    course details data use client side

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const courseDetails = require('./data/courseDetails.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courses)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courseDetails.find(details => details.id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
